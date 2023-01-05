f5fcdbc74e0316a25a67655b725e63ae0c061288
iuliasugaru <iulia.sugaru@yahoo.com>
2023-01-05 17:15:32 +0100

    Add project files.

---
+++ Sugaru_Iulia_Lab11/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Sugaru_Iulia_Lab11.Data;
+
+namespace Sugaru_Iulia_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+    }
+}
